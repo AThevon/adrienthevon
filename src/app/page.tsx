eaa08f6f13a740dfa647620fc31be0b0b2eb21e0
AThevon <adrienthevon@gmail.com>
2026-04-07 04:53:04 +0200

    fix: remove SectionEffects particle trail from homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,11 @@
 "use client";
 
-import { useState, useEffect } from "react";
-import dynamic from "next/dynamic";
 import Hero from "@/components/sections/Hero";
-import { usePerformance } from "@/hooks";
-
-const SectionEffects = dynamic(
-  () => import("@/components/effects/SectionEffects"),
-  { ssr: false }
-);
 
 export default function Home() {
-  const { enableCursorEffects } = usePerformance();
-  const [isMounted, setIsMounted] = useState(false);
-
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
-
   return (
-    <>
-      {isMounted && enableCursorEffects && <SectionEffects enabled />}
-
-      <main className="relative">
-        <Hero />
-      </main>
-    </>
+    <main className="relative">
+      <Hero />
+    </main>
   );
 }
